fdc10037a33f691333e5e62db42b7cd0eae72796
riaurko <riaurko@gmail.com>
2026-02-02 03:45:11 +0600

    first commit

---
+++ Module-1 [JS Recap and Basic ES6]/ES6 Intro/let-const.js
@@ -0,0 +1,13 @@
+const name = "Bangladesh";
+const countryName = "BD" + name;
+console.log(countryName);
+let price = 120;
+price = 150;
+console.log(price);
+
+//! const value can not be changed but we can do some operations:
+
+//? Push
+const dress = ["shirt", "pant", "jacket"];
+dress.push("mafler", "shocks");
+console.log(dress);
