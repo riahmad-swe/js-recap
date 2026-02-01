848badc0299265079a41a376f3b99a33a276dd22
riaurko <riaurko@gmail.com>
2026-02-02 05:31:13 +0600

    learn: more-arrow function

---
+++ Module-1 [JS Recap and Basic ES6]/ES6 Intro/more-arrow.js
@@ -0,0 +1,14 @@
+//! Tax calculation
+const getTax = (amount, taxRate) => (amount * taxRate) / 100;
+
+const add = (p, q) => p + q;
+
+//! single parameter use
+const getSquare = (x) => x * x;
+const getHalf = (num) => num / 2;
+console.log(getSquare(9));
+console.log(getHalf(20));
+
+//! array declaration
+const firstElement = (nums) => nums[0];
+console.log(firstElement([345, 34, 534, 45, 85]));
