a5bbc06653849177d9c85ab925c8f87f45c77520
riaurko <riaurko@gmail.com>
2026-02-02 07:25:37 +0600

    learn:entries of js

---
+++ Module-1 [JS Recap and Basic ES6]/entries.js
@@ -0,0 +1,14 @@
+const employee = {
+	name: "Rahimatullah",
+	designation: "QA",
+	salary: "20000",
+	experience: 1,
+};
+
+const keys = Object.keys(employee);
+console.log(keys);
+const values = Object.values(employee);
+console.log(values);
+
+const enries = Object.entries(employee);
+console.log(enries);
